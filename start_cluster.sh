977de58b0588c93fadafd4e8f96b2a43f2d5df34
mbodtke <mbodtke@gmail.com>
2018-10-08 23:43:32 -0500

    Create start_cluster.sh

---
+++ start_cluster.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#This script sends magic packets to wake the nodes
+#Replace <XXX> with your ethernet device
+#Replace <macADDRX> with the mac address of each node's ethernet device
+#Wake the cluster by running:
+#sudo ./start_cluster.sh
+
+#node 1
+ether-wake -i <XXX> <macADR1>
+
+#node2
+ether-wake -i <XXX> <macADR2>
+
+#node3
+#ether-wake -i <XXX> <macADR3>
